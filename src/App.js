29ee6e52dbf829ec7f4d36732b17e60faf3a1d2c
evelyndrake <evelyndrake90@gmail.com>
2023-07-31 13:47:21 -0400

    Fixed packages, added API key, added search box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from 'react';
+import { createRoot } from 'react-dom/client';
+require ('dotenv').config();
 
-function App() {
+
+
+const App = () => {
+  const inputRef = React.createRef();
+  const search = () => {
+    console.log(inputRef.current.value);
+  }
+
+
+  const SerpApi = require('google-search-results-nodejs');
+  const key = process.env.REACT_APP_API_KEY;
+  console.log(key);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <input type="text" ref={inputRef} id="search" name="search" placeholder="Search.."></input>
+        <button type="button" onClick={search}>Search</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,4 +39,5 @@ function App() {
   );
 }
 
+// export default App;
 export default App;
