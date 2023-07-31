87408148b41e9097cdc2c37f6ddddf5a1fb28ac1
evelyndrake <evelyndrake90@gmail.com>
2023-07-31 16:23:24 -0400

    Initial functionality

--- src/App.js
+++ src/App.js
@@ -7,33 +7,110 @@ require ('dotenv').config();
 
 
 const App = () => {
-  const inputRef = React.createRef();
-  const search = () => {
-    console.log(inputRef.current.value);
+  var json = {
+    "products": []
   }
 
+  
 
+  var cors = require('cors');
+  var codec = require('json-url')('lzw');
   const SerpApi = require('google-search-results-nodejs');
   const key = process.env.REACT_APP_API_KEY;
-  console.log(key);
+  
+  const inputRef = React.createRef();
+  const productListRef = React.createRef();
+  const searchResultsRef = React.createRef();
+  let ignore = false;
+  useEffect(() => {
+    
+    if (!ignore) {
+      console.log("loading");
+      productListRef.current.innerHTML = "";
+      const encoded = urlParameters.get("products");
+      if (encoded !== null) {
+        productListRef.current.innerHTML = "";
+        var decoded = codec.decompress(encoded).then(json1 => {
+          console.log(json1);
+          json = json1;
+          for (var i = 0; i < json1["products"].length; i++) {
+            productListRef.current.innerHTML += "<li><a href="+json1["products"][i]["link"]+" target='_blank'>" + json1["products"][i]["name"] + "</a></li>";
+          }
+        });
+      }
+      return () => {ignore = true;}
+    }
+  }, []);
+
+  const search = () => {
+    console.log(inputRef.current.value);
+    const searchCurrent = new SerpApi.GoogleSearch(key);
+    const params = {
+      engine: "google_shopping",
+      q: inputRef.current.value,
+      gl: "us",
+      hl: "en"
+    };
+    const callback = function(data) {
+      for (var i = 0; i < data["shopping_results"].length; i++) {
+        // searchResultsRef.current.innerHTML += "<li>" + data["shopping_results"][i]["title"];
+        addResultButton(data["shopping_results"][i]["title"], data["shopping_results"][i]["link"], data["shopping_results"][i]["thumbnail"]);
+        
+      }
+      console.log(data["shopping_results"]);
+    }
+    const addResult = (result, link) => {
+      productListRef.current.innerHTML += "<li><a href="+link+" target='_blank'>" + result + "</a></li>";
+      json["products"].push({"name": result, "link": link});
+      
+      var output = codec.compress(json).then(result => {
+        urlParameters.set("products", result);
+        window.location.search = urlParameters.toString();
+        console.log(result);});
+      
+      
+
+  }
+    searchCurrent.json(params, callback);
+    // add to search results as link
+    const addResultButton = (text, link, image) => {
+      var button = document.createElement("button");
+      // call addResult on button click
+      button.addEventListener("click", function() {
+        addResult(this.innerText, link);
+      });
+      button.innerText = text;
+      var li = document.createElement("li");
+      li.appendChild(button);
+      li.appendChild(document.createElement("br"));
+      var imageElement = document.createElement("img");
+      imageElement.src = image;
+      li.appendChild(imageElement);
+      searchResultsRef.current.appendChild(li);
+    }
+    
+    // 
+    // window.location.search += "&type=" + inputRef.current.value;
+  }
+
+  const urlParameters = new URLSearchParams(window.location.search);
+  
   
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Current products</p>
+        <ul ref={productListRef}>
+
+        </ul>
+        
+        <p>Search for a product</p>
         <input type="text" ref={inputRef} id="search" name="search" placeholder="Search.."></input>
         <button type="button" onClick={search}>Search</button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul ref={searchResultsRef}>
+
+        </ul>
       </header>
     </div>
   );
